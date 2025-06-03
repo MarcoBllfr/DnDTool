31491e8107c08e295779cfd786d69ecafaba7076
marcobllfr <122992858+Eramood@users.noreply.github.com>
2025-06-03 16:04:17 +0200

    add the challenge Rating experience

--- src/lib/data/LevelRules.ts
+++ src/lib/data/LevelRules.ts
@@ -1,3 +1,4 @@
+//levelRules PlayerLevel:XP this list is only for player (hero)
 export const levelRulesFacile: Record<number, number> = {
   1: 25,
   2: 50,
@@ -98,3 +99,45 @@ export const levelRules = {
 };
 
 export type Difficolta = keyof typeof levelRules;
+
+//this list it's the CR(Challenge Rating) for the monsters
+export const challengeRatingXP: Record<string, number> = {
+  "0": 10,      
+  "1/8": 25,      
+  "1/4": 50,     
+  "1/2": 100,    
+  "1": 200,       
+  "2": 450,       
+  "3": 700,       
+  "4": 1100,      
+  "5": 1800,      
+  "6": 2300,     
+  "7": 2900,       
+  "8": 3900,     
+  "9": 5000,     
+  "10": 5900,    
+  "11": 7200,   
+  "12": 8400,     
+  "13": 10000,    
+  "14": 11500,    
+  "15": 13000,    
+  "16": 15000,   
+  "17": 18000,    
+  "18": 20000,    
+  "19": 22000,    
+  "20": 25000,    
+  "21": 33000,    
+  "22": 41000,    
+  "23": 50000,   
+  "24": 62000,    
+  "25": 75000,   
+  "26": 90000,    
+  "27": 105000,   
+  "28": 120000,   
+  "29": 135000,  
+  "30": 155000,   
+};
+
+export function getCrExperience(cr: string): number {
+  return challengeRatingXP[cr] || 0;
+}
